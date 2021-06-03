3d3dc406070006e8483f004326dfdcf12ac7b83d
English-For-Life <83182532+English-For-Life@users.noreply.github.com>
2021-06-03 23:57:43 +0300

    Add files via upload

---
+++ questions.js
@@ -0,0 +1,54 @@
+//Array of objects
+const quiz = [
+    {
+        q:"I always feel tired ....... the evening.",
+        options:["in", "on", "at", "-"],
+        answer: 0
+    },
+    {
+        q:"Will you be at home ....... this evening?",
+        options:["in", "-", "on", "at"],
+        answer: 1
+    },
+    {
+        q:"We went to France ....... last summer.",
+        options:["in", "at", "-", "on"],
+        answer: 2
+    },
+    {
+        q:"Laura was born ....... 1997.",
+        options:["-", "at", "on", "in" ],
+        answer: 3
+    },
+    {
+        q:"What are you doing ....... the weekend?",
+        options:["at", "in", "on", "-"],
+        answer: 0
+    },
+    {
+        q:"I phone Robert ....... every Sunday.",
+        options:["in", "-", "on", "at"],
+        answer: 1
+    },
+    {
+        q:"Will we play tennis ....... next Sunday?",
+        options:["at", "in", "-", "on"],
+        answer: 2   
+    },
+    {
+        q:"I can’t go to the party ....... Sunday.",
+        options:["at", "in", "-", "on"],
+        answer: 3
+    },
+    {
+        q:"I’m going out. I’ll be back ....... an hour.",
+        options:["in", "on", "at", "-"],
+        answer: 0
+    },
+    {
+        q:"I don’t often go out ....... night.",
+        options:["in", "at", "on", "-"],
+        answer: 1
+    }
+
+]
